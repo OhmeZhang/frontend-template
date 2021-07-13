5e47632d8518353b8934be6d42f358d7be79a796
OhmeZhang <OhmeZhang@aliyun.com>
2021-07-13 14:09:10 +0800

    feat(main-app): 框架搭建

--- main-app/src/App.vue
+++ main-app/src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>
-            |
-            <router-link to="/about">About</router-link>
-        </div>
-        <router-view/>
+        <Layout/>
     </div>
 </template>
 
 <script>
-export default {};
+    import Layout from '@/layout';
+
+    export default {
+        name: 'App',
+
+        components: {
+            Layout
+        }
+    };
 </script>
 
 <style lang="scss">
@@ -20,18 +23,5 @@ export default {};
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
 }
 </style>
